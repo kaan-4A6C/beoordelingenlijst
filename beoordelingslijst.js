9e879b7e426d4c25307fd2b7278b5e39264c35ba
kaan-4A6C <510507@edu.rocmn.nl>
2017-02-23 10:42:10 +0100

    just chicken

---
+++ beoordelingslijst.js
@@ -0,0 +1,21 @@
+function bereken(input){
+  var beoordeling;
+  if (input >= 0 && input <= 49) {
+    beoordeling = " onvoldoende";
+  } else if (input >= 50 && input <= 59) {
+    beoordeling = " matig";
+  } else if (input >= 60 && input <= 74) {
+    beoordeling = " voldoende";
+  } else if (input >= 75 && input <= 100) {
+    beoordeling = " goed";
+  }
+  return beoordeling;
+}
+function cijfer(){
+  var input = document.getElementById('input').value;
+    document.getElementById('resultaat').innerHTML += "Het cijfer is "+ input +" en is dus" + bereken(input) + "<br>";
+}
+function cijfer1(){
+  var input = document.getElementById('input1').value;
+    document.getElementById('resultaat').innerHTML += 'de beoordeling is' + bereken(input) + ' , want het cijfer is: ' + input + "<br>";
+}
